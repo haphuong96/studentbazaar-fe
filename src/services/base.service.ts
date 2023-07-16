a2d4778d8139a969d93ef3c498b7f1476c9ff03a
phuong <tnguyen09@qub.ac.uk>
2023-07-16 07:14:50 +0700

    [STUD-12] Browse Product + add Axios interceptors

--- src/services/base.service.ts
+++ src/services/base.service.ts
@@ -1,6 +1,56 @@
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
 import { envConfigs } from "../configs/env-configs";
+import { localStorageKeys } from "../common/local-storage-keys";
 
 export const axiosInstance = axios.create({
   baseURL: envConfigs.BASE_API,
 });
+
+// Add a request interceptor, attach token to authorization header
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem(localStorageKeys.ACCESS_TOKEN);
+
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+// Add a response interceptor, refresh token if expired
+const handleResponseSuccess = (
+  response: AxiosResponse<any, any>
+): AxiosResponse<any, any> => {
+  return response;
+};
+const handleResponseError = async (error: any) => {
+  // https://axios-http.com/docs/res_schema
+  const originalRequest = error.config;
+
+  if (error.response.status === 401) {
+    const refreshToken = localStorage.getItem(localStorageKeys.REFRESH_TOKEN);
+
+    const res = await axiosInstance.post("auth/refresh-token", {
+      refreshToken,
+    });
+
+    if (res.status === 201) {
+      localStorage.setItem(localStorageKeys.ACCESS_TOKEN, res.data.accessToken);
+      localStorage.setItem(localStorageKeys.REFRESH_TOKEN, res.data.refreshToken);
+    }
+
+    return axiosInstance(originalRequest);
+  }
+
+  return Promise.reject(error);
+};
+
+axiosInstance.interceptors.response.use(
+  handleResponseSuccess,
+  handleResponseError
+);
--- src/views/item/Marketplace.vue
+++ src/views/item/Marketplace.vue
@@ -1,34 +1,34 @@
 <script setup lang="ts">
 import { onMounted, ref } from "vue";
-import {ItemService} from "../../services/item.service";
+import { ItemService } from "../../services/item.service";
 
 const itemList = ref();
 
-onMounted(() => {
-    getItems();
-})
+onMounted(async () => {
+  getItems();
+});
+
 const getItems = async () => {
-    try {
-        const data = await ItemService.getItems();
-        itemList.value = data;
-        console.log('itemList',itemList.value)
-    } catch(err) {
-        console.log(err)
-    }
-}
+  try {
+    const data = await ItemService.getItems();
+    itemList.value = data;
+    console.log("itemList", itemList.value);
+  } catch (err) {
+    console.log(err);
+  }
+};
 </script>
 <template>
   <h2>Listed recently</h2>
-  <div v-for="item in itemList" v-if="itemList">
-    {{ item.itemName }}
-    
-  </div>
   <a-row :gutter="[16, 16]">
-    <a-col :span="6" >test1test1test1test1test1test1test1test1test1test1</a-col>
-    <a-col :span="6" >test2</a-col>
-    <a-col :span="6" >test3</a-col>
-    <a-col :span="6" >test4</a-col>
+    <a-col :span="6" v-for="item in itemList" v-if="itemList">
+      <div>{{ item.owner }}</div>
+      <div class="p-16">
+        <a-skeleton-image class="img"></a-skeleton-image>
+      </div>
+      <div>{{ item.itemName }}</div></a-col
+    >
   </a-row>
 </template>
-
+<style></style>
 <!-- class="logo" -->
